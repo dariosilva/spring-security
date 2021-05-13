0229337c74a1f7a74eb46ed34d75ee57e48d4ca2
Dário Silva <dario.silva@vilt-group.com>
2021-05-13 23:12:56 +0100

    Added Two Factor Authentication.

---
+++ sec16/src/main/java/guru/sfg/brewery/config/SecurityBeans.java
@@ -0,0 +1,51 @@
+package guru.sfg.brewery.config;
+
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorConfig;
+import com.warrenstrange.googleauth.ICredentialRepository;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by jt on 7/17/20.
+ */
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public GoogleAuthenticator googleAuthenticator(ICredentialRepository credentialRepository){
+        GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder configBuilder
+                = new GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder();
+
+        configBuilder
+                .setTimeStepSizeInMillis(TimeUnit.SECONDS.toMillis(60))
+                .setWindowSize(10)
+                .setNumberOfScratchCodes(0);
+
+        GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator(configBuilder.build());
+        googleAuthenticator.setCredentialRepository(credentialRepository);
+        return googleAuthenticator;
+
+    }
+
+    @Bean
+    public AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher applicationEventPublisher){
+        return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(DataSource dataSource){
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
+    }
+
+}
